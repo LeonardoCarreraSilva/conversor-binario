c2c890290ddfa227147731ea53471f255a3c4618
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2021-08-07 18:57:49 -0300

    Criado main e feito grande parte do frontEnd da aplicacao

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  Container,
+  FormControl,
+  InputLabel,
+  Input,
+} from "@material-ui/core";
 
-export default function Main(){
-  return(
-    <div>
+export default function Main() {
+  const [binary, setBinary] = useState();
+  const [result, setResult] = useState();
+
+  return (
+    <Container maxWidth="sm">
       <h1>Concersor de binario para decimal</h1>
+      <FormControl margin="normal" fullWidth>
+        <InputLabel>Binary</InputLabel>
+        <Input
+          id="binary"
+          fullWidth
+          value={this.binary}
+          onChange={() => {
+            //fazer logica
+          }}
+        />
+      </FormControl>
 
-    </div>
-  )
+      <FormControl disabled margin="normal" fullWidth>
+        <InputLabel htmlFor="component-disable">Resultado Decimal</InputLabel>
+        <Input
+          id="result"
+          value={this.result}
+          onChange={() => {
+            //fazer logica
+          }}
+        />
+      </FormControl>
+    </Container>
+  );
 }
