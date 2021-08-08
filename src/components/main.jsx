bad2aaeedea697c0640542639a91a0f5e4c4579a
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2021-08-08 18:07:40 -0300

    Finalizao logica de calculo

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,39 +1,47 @@
 import React, { useState } from "react";
-import {
-  Container,
-  FormControl,
-  InputLabel,
-  Input,
-} from "@material-ui/core";
+
+import { Container, FormControl, InputLabel, Input } from "@material-ui/core";
+
+function getArray(binario) {
+  const string = `${binario}`;
+  const array = string.split("").reverse();
+  return array;
+}
 
 export default function Main() {
-  const [binary, setBinary] = useState();
-  const [result, setResult] = useState();
+  const [binary, setBinary] = useState([]);
+  const [result, setResult] = useState(0);
+
+  function conversor(string) {
+    var resultado = 0;
+    var array = getArray(string);
+    var espoenteMaximo = (parseInt(string.length) - 1) ;
+    var aux = 0;
+
+    for (let i = 0; i <= espoenteMaximo; i++) {
+      aux = Math.pow(2, parseInt(i)) * parseInt(array[i]);
+      resultado = resultado + aux;
+    }
+    setResult(resultado);
+  }
 
   return (
     <Container maxWidth="sm">
-      <h1>Concersor de binario para decimal</h1>
+      <h1>Conversor de binario para decimal</h1>
       <FormControl margin="normal" fullWidth>
-        <InputLabel>Binary</InputLabel>
+        <InputLabel>Binario</InputLabel>
         <Input
           id="binary"
           fullWidth
-          value={this.binary}
-          onChange={() => {
-            //fazer logica
+          value={binary}
+          onChange={(event) => {
+            setBinary(event.target.value);
+            conversor(event.target.value);
           }}
         />
       </FormControl>
-
-      <FormControl disabled margin="normal" fullWidth>
-        <InputLabel htmlFor="component-disable">Resultado Decimal</InputLabel>
-        <Input
-          id="result"
-          value={this.result}
-          onChange={() => {
-            //fazer logica
-          }}
-        />
+      <FormControl fullWidth>
+        <Input id="result" value={result} disabled/>
       </FormControl>
     </Container>
   );
